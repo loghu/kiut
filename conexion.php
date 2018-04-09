25036ed062fbac16628889985e1bfb42dee75e0e
hugo lopez <loghu06@gmail.com>
2018-04-09 12:44:28 -0500

    archivo conexion a base de datos

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+
+$servidor="localhost";
+$usuario="root";
+$clave="";
+$base="hola";
+  $con=mysqli_connect($servidor, $usuario, $clave, $base);
+  ?>
--- login.php
+++ login.php
@@ -5,7 +5,13 @@
     <title>login</title>
   </head>
   <body>
-    hola
-    
+    <div class="contenedor">
+      <form class="" action="hola.php" method="post">
+        Bienvenido <br>
+        <input type="email" name="mail" class="caja" placeholder="Correo Electronico" required="true"><br>
+        <input type="password" name="pas" class="caja" value="" placeholder="Contraseña" required="true"><br>
+        <input type="submit" name="act" value="Enviar" class="boton"><br>
+      </form>
+    </div>
   </body>
 </html>
