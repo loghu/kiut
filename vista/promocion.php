72033002e7cad2ad280602b43c86e52dbb7ff178
SavedRS <45495400+SavedRS@users.noreply.github.com>
2018-12-06 15:47:44 -0600

    Update promocion.php

--- vista/promocion.php
+++ vista/promocion.php
@@ -2,9 +2,19 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title> PROMOCIONES </title>
+    <title> PROMOCIONES    </title>
   </head>
+
   <body>
+
+    <nav class="navbar navbar-default" role="navigation">
+   <div class="collapse navbar-collapse navbar-ex1-collapse">
+     <ul class="nav navbar-nav">
+       <li><a href="home.php">Menu</a></li>
+       <li><a href="venta.php">Venta</a></li>
+       <li><a href="promo.php">Promociones</a></li>
+   </div>
+ </nav>
 
   </body>
 </html>
